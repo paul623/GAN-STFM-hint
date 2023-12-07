90cec5a1a913a06c6627e1d05a5d7dd4dba08c87
巴塞罗那的余晖 <zhubaoluo@outlook.com>
2023-12-07 17:00:14 +0800

    更新评价指标

--- learning/evaluatingPoint.py
+++ learning/evaluatingPoint.py
@@ -9,6 +9,7 @@ import numpy as np
 import rasterio
 import torch
 from numpy.linalg import norm
+import tifffile
 from pytorch_msssim import ssim
 
 ground_truth_dir = "/home/byt/Fusion/ganstfm-main/ganstfm-main/dataset/LGC_data/refs/20050129_TM.tif"
@@ -44,38 +45,44 @@ def calculate_rmse(image_pred, image_target):
 
     return rmse.item()
 
-def calculate_sam(x_pred, x_true):
-    # 初始化 SAM 结果
-    sam_deg = 0.0
-    total_pixels = 0
+def spectral_angle_mapper(spectrum1, spectrum2):
+    # 归一化两个光谱向量
+    spectrum1_normalized = spectrum1 / np.linalg.norm(spectrum1)
+    spectrum2_normalized = spectrum2 / np.linalg.norm(spectrum2)
 
-    # 读取预测和真实图像数据
-    with rasterio.open(x_pred) as src_pred, rasterio.open(x_true) as src_true:
-        # 读取数据并将其转换为浮点型
-        image_data_pred = src_pred.read().astype(np.float32)
-        image_data_true = src_true.read().astype(np.float32)
+    # 计算夹角余弦值
+    cosine_angle = np.dot(spectrum1_normalized, spectrum2_normalized)
 
-        # 调整通道顺序
-        image_data_pred = np.transpose(image_data_pred, (1, 2, 0))
-        image_data_true = np.transpose(image_data_true, (1, 2, 0))
+    # 计算夹角
+    angle = np.arccos(cosine_angle)
 
-        # 获取图像高度和宽度
-        height, width, channels = image_data_pred.shape
+    # 将弧度转换为度
+    angle_degrees = np.degrees(angle)
 
-        # 计算每个像素点的 SAM
-        for i in range(height):
-            for j in range(width):
-                vec_pred = image_data_pred[i, j]
-                vec_true = image_data_true[i, j]
+    return angle_degrees
 
-                # 计算夹角
-                sam_rad = np.arccos(np.dot(vec_pred, vec_true) / (norm(vec_pred) * norm(vec_true)))
-                sam_deg += np.degrees(sam_rad)
-                total_pixels += 1
+def calculate_sam(image1_path, image2_path):
+    # 通过 tifffile 读取 TIFF 格式图像
+    image1 = tifffile.imread(image1_path)
+    image2 = tifffile.imread(image2_path)
+
+    # 获取图像的光谱向量（每个像素点）
+    spectra1 = image1.reshape((-1, image1.shape[-1]))
+    spectra2 = image2.reshape((-1, image2.shape[-1]))
+
+    # 初始化 SAM 值
+    sam_values = []
+
+    # 计算 SAM
+    for spec1, spec2 in zip(spectra1, spectra2):
+        sam = spectral_angle_mapper(spec1, spec2)
+        sam_values.append(sam)
 
     # 计算平均 SAM
-    sam_deg /= total_pixels
-    return sam_deg
+    average_sam = np.mean(sam_values)
+
+    return average_sam
+
 
 def calculate_ssim(image_pred, image_target):
     return ssim(image_pred, image_target, data_range=255, size_average=False)
@@ -85,12 +92,12 @@ image_pred = load_tiff_as_tensor(predict_dir)
 image_target = load_tiff_as_tensor(ground_truth_dir)
 
 
-mae_value = calculate_mae(image_pred, image_target)
-rmse_value = calculate_rmse(image_pred, image_target)
-# sam_value = calculate_sam(predict_dir, ground_truth_dir)
-ssim_value = calculate_ssim(image_pred, image_target)
+mae_value = calculate_mae(image_pred, image_pred)
+rmse_value = calculate_rmse(image_pred, image_pred)
+sam_value = calculate_sam(predict_dir, predict_dir)
+ssim_value = calculate_ssim(image_pred, image_pred)
 
 print(f"Mean Absolute Error (MAE): {mae_value:.4f}")
 print(f"Root Mean Square Error (RMSE): {rmse_value:.4f}")
-# print(f"Spectral Angle Mapper(SAM): {sam_value:.4f}")
+print(f"Spectral Angle Mapper(SAM): {sam_value:.4f}")
 print(f"Structural Similarity Index (SSIM): {ssim_value.item():.4f}")
